973229c025ad30f50fe477f15c3d107a0a620cc1
Islam Gomaa <iislamgom3a@gmail.com>
2025-12-01 22:29:39 +0200

    update nvim config

--- nvim/.config/nvim/lua/core/options.lua
+++ nvim/.config/nvim/lua/core/options.lua
@@ -1,12 +1,13 @@
-vim.o.number = true
-vim.o.relativenumber = true
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
 vim.o.wrap = true
 vim.o.shiftwidth = 4
 vim.o.tabstop = 4
 vim.o.swapfile = false
 vim.o.autoindent = true
-vim.o.clipboard = 'unnamedplus'
-vim.o.mouse = 'a' 
-vim.o.writebackup = false 
+vim.o.clipboard = "unnamedplus"
+vim.o.mouse = "a"
+vim.o.writebackup = false
 vim.o.scrolloff = 15
-
+vim.o.colorcolumn = "100"
