a4c3b0701f9385394ee776a039b930cda66b11ef
Islam Gomaa <iislamgom3a@gmail.com>
2025-12-01 18:50:27 +0200

    added: terminal syntax-highlighting, autosuggestion

--- zsh/.config/zsh/custom.zsh
+++ zsh/.config/zsh/custom.zsh
@@ -1,9 +1,15 @@
-# Binds Ctrl+H (or Backspace) to delete the previous word.
-bindkey '^H' backward-kill-word
-
-
 # Starship
 export STARSHIP_CONFIG="$HOME/.config/starship/starship.toml"
 eval "$(starship init zsh)"
 
+
+# Syntax highlighting
+source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+(( ${+ZSH_HIGHLIGHT_STYLES} )) || typeset -A ZSH_HIGHLIGHT_STYLES
+ZSH_HIGHLIGHT_STYLES[path]=none
+ZSH_HIGHLIGHT_STYLES[path_prefix]=none
+
+# Autosuggestions
+source /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh
 
