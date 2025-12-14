84ecac7713f755131deb916874a5667b807b28a4
Islam Gomaa <iislamgom3a@gmail.com>
2025-12-14 15:53:51 +0200

    modified: transparency in everforert theme

--- nvim/.config/nvim/lua/plugins/everforest.lua
+++ nvim/.config/nvim/lua/plugins/everforest.lua
@@ -8,7 +8,7 @@ function M.config()
 	local everforest = require("everforest")
 	everforest.setup({
 		background = "hard",
-		transparent_background_level = 1,
+		transparent_background_level = 0,
 		italics = true,
 		disable_italic_comments = false,
 		inlay_hints_background = "dimmed",
