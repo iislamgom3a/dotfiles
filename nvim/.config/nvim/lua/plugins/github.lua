a308a45efe13351e40ad827fadf7cd88f9d3332e
Islam Gomaa <iislamgom3a@gmail.com>
2026-01-26 23:01:40 +0200

    sync

--- nvim/.config/nvim/lua/plugins/github.lua
+++ nvim/.config/nvim/lua/plugins/github.lua
@@ -1,13 +1,10 @@
 return {
-	"projekt0n/github-nvim-theme",
-	name = "github-theme",
-	lazy = false,
-	priority = 1000,
-	config = function()
-		require("github-theme").setup({
-			options = {
-				transparent = true, -- Disable setting bg (make neovim's background transparent)
-			},
-		})
-	end,
+    "projekt0n/github-nvim-theme",
+    name = "github-theme",
+    lazy = false,
+    priority = 1000,
+    config = function()
+        require("github-theme").setup({
+        })
+    end,
 }
