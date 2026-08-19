c00d838e1938c3f69a1bd697a872bb4efacd4232
Islam Gomaa <iislamgom3a@gmail.com>
2026-08-19 09:11:53 +0300

    sync

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -5,11 +5,12 @@ return {
 	event = "BufReadPost",
 	opts = {
 		suggestion = {
-			enabled = not vim.g.ai_cmp,
+			-- enabled = not vim.g.ai_cmp,
+			enabled = true,
 			auto_trigger = true,
-			hide_during_completion = vim.g.ai_cmp,
+			-- hide_during_completion = vim.g.ai_cmp,
 			keymap = {
-				accept = false, -- handled by nvim-cmp / blink.cmp
+				accept = "<Tab>", -- handled by nvim-cmp / blink.cmp
 				next = "<M-]>",
 				prev = "<M-[>",
 			},
